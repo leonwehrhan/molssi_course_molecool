19f7f10891f24bea5b1d9cc5dea4a0a7817134c8
Leon Wehrhan <mail@leonwehrhan.de>
2020-05-30 09:36:10 +0200

    test calculate_angle

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -15,3 +15,15 @@ def test_calculate_distance():
     calculated_distance = molecool.calculate_distance(r1, r2)
 
     assert calculated_distance == expected_distance
+
+def test_calculate_angle():
+
+    rA = np.array([0, 0, 1])
+    rB = np.array([0, 0, 0])
+    rC = np.array([0, 1, 0])
+
+    expected_angle = 90. # in degrees
+
+    calculated_angle = molecool.calculate_angle(rA, rB, rC, degrees=True)
+
+    assert calculated_angle == expected_angle
