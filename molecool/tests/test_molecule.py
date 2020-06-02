65d407abb36bad3843a5adf6bf7f269a1c21ddb3
Leon Wehrhan <mail@leonwehrhan.de>
2020-06-02 17:42:04 +0200

    test calculate_center_of_mass

--- molecool/tests/test_molecule.py
+++ molecool/tests/test_molecule.py
@@ -31,3 +31,13 @@ def test_molecular_mass():
 
     assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
 
+
+def test_center_of_mass():
+    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
+    coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+
+    center_of_mass = molecool.calculate_center_of_mass(symbols, coordinates)
+
+    expected_center = np.array([1,1,1])
+
+    assert np.array_equal(center_of_mass, expected_center)
