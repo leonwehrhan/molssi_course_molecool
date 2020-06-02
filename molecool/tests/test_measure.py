1dc252f8e79f0af1f9b4a67c646b0de9c7dd933e
Leon Wehrhan <mail@leonwehrhan.de>
2020-06-02 20:48:04 +0200

    parametrize

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -4,6 +4,8 @@ Unit and regression test for the measure module
 
 import molecool
 import numpy as np
+import pytest
+
 
 def test_calculate_distance():
 
@@ -16,13 +18,20 @@ def test_calculate_distance():
 
     assert calculated_distance == expected_distance
 
-def test_calculate_angle():
+@pytest.mark.parametrize(
+'rA, rB, rC, expected_angle',
+[
+(np.array([0, 0, 1]), np.array([0, 0, 0]), np.array([0, 1, 0]), 90.),
+(np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 45.)
+]
+)
+def test_calculate_angle(rA, rB, rC, expected_angle):
 
-    rA = np.array([0, 0, 1])
-    rB = np.array([0, 0, 0])
-    rC = np.array([0, 1, 0])
+    #rA = np.array([0, 0, 1])
+    #rB = np.array([0, 0, 0])
+    #rC = np.array([0, 1, 0])
 
-    expected_angle = 90. # in degrees
+    #expected_angle = 90. # in degrees
 
     calculated_angle = molecool.calculate_angle(rA, rB, rC, degrees=True)
 
