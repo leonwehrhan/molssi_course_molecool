1028fdb3fc70c35732da04c35f12388310264929
Leon Wehrhan <mail@leonwehrhan.de>
2020-06-02 20:31:16 +0200

    fixtures

--- molecool/tests/test_molecule.py
+++ molecool/tests/test_molecule.py
@@ -6,7 +6,11 @@ import molecool
 import numpy as np
 import pytest
 
-def test_build_bond_list():
+
+@pytest.fixture
+def methane_molecule():
+
+    symbols = ['C', 'H', 'H', 'H', 'H']
     coordinates = np.array([
     [1, 1, 1],
     [2.4, 1, 1],
@@ -15,6 +19,18 @@ def test_build_bond_list():
     [1, 1, -0.4]
     ])
 
+    return symbols, coordinates
+
+#def test_move_methane(methane_molecule):
+#    #demostration of @pytest.fixture(scope='module')
+#    symbols, coordinates = methane_molecule
+#
+#    coordinates[0] += 5
+
+def test_build_bond_list(methane_molecule):
+
+    symbols, coordinates = methane_molecule
+
     bonds = molecool.build_bond_list(coordinates)
 
     assert len(bonds) == 4
@@ -22,8 +38,10 @@ def test_build_bond_list():
     for bond_length in bonds.values():
         assert bond_length == 1.4
 
-def test_molecular_mass():
-    symbols = ['C', 'H', 'H', 'H', 'H']
+def test_molecular_mass(methane_molecule):
+
+    symbols, coordinates = methane_molecule
+    #symbols = ['C', 'H', 'H', 'H', 'H']
 
     calculated_mass = molecool.calculate_molecular_mass(symbols)
 
@@ -32,9 +50,9 @@ def test_molecular_mass():
     assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
 
 
-def test_center_of_mass():
-    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
-    coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+def test_center_of_mass(methane_molecule):
+
+    symbols, coordinates = methane_molecule
 
     center_of_mass = molecool.calculate_center_of_mass(symbols, coordinates)
 
