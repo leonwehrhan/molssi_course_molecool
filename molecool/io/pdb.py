cbd6b8d9bdaaf707e602b096b62c0d9532101143
Leon Wehrhan <mail@leonwehrhan.de>
2020-06-02 16:51:59 +0200

    Catch ValueError in open_pdb

--- molecool/io/pdb.py
+++ molecool/io/pdb.py
@@ -15,7 +15,10 @@ def open_pdb(f_loc):
     for l in data:
         if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
             sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
+            try:
+                c2 = [float(x) for x in l[30:55].split()]
+            except ValueError as e:
+                raise ValueError(f'Error in pdb file {f_loc}. Could not read coordinates.\n {e}')
             c.append(c2)
     coords = np.array(c)
     return sym, coords
